1e8e9a3c216170d31fd512c5b0eff5b9d06350e1
nick russo <nick.russo@bbdo.com>
2019-12-12 16:51:06 -0500

    memoized recursive factorial

--- factorial.js
+++ factorial.js
@@ -1,11 +1,39 @@
-const factorial = (n)=>{
-    // console.log(n)
-    if(n === 1){
-        return n;   
+//not memoed
+// const factorial = (n)=>{
+//     // console.log(n)
+//     if(n === 1){
+//         return n;   
+//     }
+//     console.log(`returning ${n} * factorial(${n - 1})`)
+//     return n * factorial((n-1));
+// }
+
+// let result = factorial(8);
+// console.log(result);
+
+//memoized
+const memoize = (cb)=>{
+    let cache = {}
+    return (n)=>{
+        if(n in cache){
+            console.log("return from cache")
+            return cache[n];
+        }
+
+        console.log("evaluate")
+        cache[n] = cb(n);
+        return cb(n);
     }
-    console.log(`returning ${n} * factorial(${n - 1})`)
-    return n * factorial((n-1));
 }
 
-let result = factorial(8);
-console.log(result);
+let memoizedFactorial = memoize((x)=>{
+    if(x === 0){
+        return 1;
+    }
+    return x * memoizedFactorial(x-1);
+})
+
+console.log(memoizedFactorial(3))
+// console.log(memoizedFactorial(10))
+// console.log(memoizedFactorial(10))
+// console.log(memoizedFactorial(3))
